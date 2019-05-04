88d035940ab4753f63cf9b5d82a6adc4f0e07681
Micaela Saez <mica_saez@hotmail.com>
2019-05-04 07:24:57 -0300

    preguntas terminado

--- src/app/layout/preguntas/preguntas.component.ts
+++ src/app/layout/preguntas/preguntas.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import {NgbModal, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
+import { ModalComponent } from '../bs-component/components';
 
 @Component({
   selector: 'app-preguntas',
@@ -65,20 +68,20 @@ export class PreguntasComponent implements OnInit {
     {
     'id': 6 ,
     'pregunta': '¿Que puede esperar uno de la carre de licenciatura en artes?',
-    'idAlumno': 3,
+    'idAlumno': 5,
     'idProfesional': 7,
-    'publico': true,
+    'publico': false,
     'respuesta': 'La carrera universitaria de Licenciatura en Artes forma en las diferentes expresiones artísticas',
     'positivo': true,
     'tipo': 'arte',
     'fecha': '6/7/19'
     },
     {
-    'id': 7 ,
+    'id': 5 ,
     'pregunta': '¿Cuanto dura en promedio la carrera de contador publico?',
-    'idAlumno': 1,
+    'idAlumno': 5,
     'idProfesional': 2,
-    'publico': true,
+    'publico': false,
     'respuesta': '5 años y medio en promedio.',
     'positivo': true,
     'tipo': 'tecnologia',
@@ -91,11 +94,24 @@ export class PreguntasComponent implements OnInit {
     { tipo: 'arte', icono: 'fa fa-paint-brush'},
     { tipo: 'politica', icono: 'fa fa-gavel'}
   ];
-  public profesional: boolean;
-  public alumnoLoggueado = false;
-  public perfil = ''; // profesional
 
-  constructor() {
+  public preguntasAuxPublico: Array<any> = new Array<any>();
+  public preguntasAuxPersonalizado: Array<any> = new Array<any>();
+  public profesional = false;
+  public alumnoLoggueado = false;
+  public admin = false;
+  public tipo: string;
+  public showModal = false;
+
+  constructor(private actRoute: ActivatedRoute, private modalService: NgbModal) {
+    localStorage.setItem('id', '5');
+    localStorage.setItem('tipo', 'admin');
+    this.tipo = this.actRoute.snapshot.params.tipo;
+    for (const p of this.preguntas) {
+      if (p.publico) {
+        this.preguntasAuxPublico.push(p);
+      }
+    }
     let i = 0;
     for (const pregunta of this.preguntas) {
       pregunta['lado'] = i ? 'timeline-inverted' : '';
@@ -107,18 +123,42 @@ export class PreguntasComponent implements OnInit {
         }
       }
     }
-    if (this.perfil === 'profesional') {
-      this.profesional = true;
+    if (this.tipo === 'privada') {
+      const perfil = localStorage.getItem('tipo');
+      const idUsuario = parseInt(localStorage.getItem('id'), 10);
+      for (const preg of this.preguntas) {
+        if (preg.id === idUsuario) {
+          this.preguntasAuxPersonalizado.push(preg);
+        }
+      }
+      if (perfil === 'usuario') {
+        this.alumnoLoggueado = true;
+        this.preguntas = this.preguntasAuxPersonalizado;
+      } else if (perfil === 'profesional') {
+        this.profesional = true;
+        this.preguntas = this.preguntasAuxPersonalizado;
+      } else if (perfil === 'admin') {
+        const preguntasAux: Array<any> = new Array<any>();
+        for (const preg of this.preguntas) {
+          if (!preg.publico) {
+            preguntasAux.push(preg);
+          }
+        }
+        this.preguntas = preguntasAux;
+        this.admin = true;
+      }
     } else {
-      this.profesional = false;
+      this.preguntas = this.preguntasAuxPublico;
     }
-    if (this.perfil === 'usuario') {
-      this.alumnoLoggueado = true;
-    }
-
   }
 
   ngOnInit() {
   }
 
+  public GenerarPregunta() {
+    this.showModal = true;
+  }
+  public Cancelar() {
+    this.showModal = false;
+  }
 }
