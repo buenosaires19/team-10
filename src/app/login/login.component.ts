d9f2dd24998550e36ee257e788409e944bb550c2
Ezequiel Mahafud <ezemahafud@gmail.com>
2019-05-04 00:46:42 -0300

    Se setea el perfil del usuario en el localStorage al pasar por el login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -9,13 +9,32 @@ import { routerTransition } from '../router.animations';
     animations: [routerTransition()]
 })
 export class LoginComponent implements OnInit {
+    public correo: string;
+    public clave: string;
+
     constructor(
       public router: Router
-    ) {}
+    ) {
+        this.correo = '';
+        this.clave = '';
+    }
 
     ngOnInit() {}
 
     onLoggedin() {
+        // NO FUNCIONAN LOS NGMODELS
+        switch ( (<HTMLInputElement> document.getElementById('correoInput')).value ) {
+            case 'admin@gmail.com':
+                localStorage.setItem('perfil', 'admin');
+                break;
+            case 'profesor@gmail.com':
+                localStorage.setItem('perfil', 'profesor');
+                break;
+            default:
+                localStorage.setItem('perfil', 'alumno');
+                break;
+        }
+
         localStorage.setItem('isLoggedin', 'true');
     }
 }
